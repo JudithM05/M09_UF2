283953e8cba437f8fb6f0934cf9de77e86104096
JudithM05 <2023_judith.mera@iticbcn.cat>
2025-01-11 17:25:16 +0100

    03-Sleep_i_Coet commit final

--- 03-Sleep_i_Coet/src/Motor.java
+++ 03-Sleep_i_Coet/src/Motor.java
@@ -12,7 +12,7 @@ public class Motor {
         this.potenciaObjectiu = p;
         ajustarPotencia();
     }
-    
+
     public int getPotenciaActual() {
         return potenciaActual;
     }
@@ -22,26 +22,37 @@ public class Motor {
         new Thread(() -> {
             try {
                 //Mentre està en funcionament
-                while (enFuncionament && potenciaActual != potenciaObjectiu) {
+                while (enFuncionament) {
                     //S'aumenta la potència actual si és menor que la objectiu
                     if (potenciaActual < potenciaObjectiu) {
                         potenciaActual++;
-                        System.out.println("Motor " + id + ": Incre. Objectiu: " + potenciaObjectiu + " Actual: " + potenciaActual);
+                        // Només imprimeix si no s'ha arribat a l'objectiu
+                        if (potenciaActual != potenciaObjectiu) {
+                            System.out.println("Motor " + id + ": Incre. Objectiu: " + potenciaObjectiu + " Actual: " + potenciaActual);
+                        }
                     //Es decrementa la potència actual si és major que la objectiu
                     } else if (potenciaActual > potenciaObjectiu) {
                         potenciaActual--;
-                        System.out.println("Motor " + id + ": Decre. Objectiu: " + potenciaObjectiu + " Actual: " + potenciaActual);
+                        // Només imprimeix si no s'ha arribat a l'objectiu
+                        if (potenciaActual != potenciaObjectiu) {
+                            System.out.println("Motor " + id + ": Decre. Objectiu: " + potenciaObjectiu + " Actual: " + potenciaActual);
+                        }
+                    }
+
+                    // Si les potències són iguals, mostra "FerRes"
+                    if (potenciaActual == potenciaObjectiu) {
+                        System.out.println("Motor " + id + ": FerRes Objectiu: " + potenciaObjectiu + " Actual: " + potenciaActual);
+
+                        // Si les dues potències són 0 no està en funcionament
+                        if (potenciaObjectiu == 0) {
+                            enFuncionament = false;
+                        }
+                        break; // Surt del bucle perquè la potència ja és igual
                     }
 
                     // Simula l'intèrval aleatori
                     int interval = (int) (Math.random() * 1000) + 1000; // Entre 1000ms i 2000ms
                     Thread.sleep(interval);
-                }
-
-                //Si les dues potències són 0 no està en funcionament
-                if (potenciaObjectiu == 0 && potenciaActual == 0) {
-                    System.out.println("Motor " + id + ": FerRes Objectiu: " + potenciaObjectiu + " Actual: " + potenciaActual);
-                    enFuncionament = false;
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
